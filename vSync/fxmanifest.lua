ab69e96b1f4dc74a27e7090b810f6dcba90ee7de
foltone <foltonedev@gmail.com>
2024-05-11 13:35:09 +0300

    update vSync for esx group

---
+++ vSync/fxmanifest.lua
@@ -0,0 +1,13 @@
+resofx_version 'cerulean'
+game 'common'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+urce_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    'vs_client.lua',
+}
+
+server_scripts {
+    'vs_server.lua',
+}
